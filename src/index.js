ead8036f56c40e8aeeb9cce2bc2fa8ca52617bd2
ofaaoficial <oscarfamado@gmail.com>
2019-10-08 18:31:11 -0500

    system of routes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname , 'views'));
 
 
-//motor de vistas
+//View engine
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
@@ -23,12 +23,22 @@ app.set('view engine', '.hbs');
 
 //Middlewares - Security
 app.use(morgan('dev'));
+//Management of get data
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //Global variables
+app.use((req, res, next)=> {
+    next();
+});
 
 //Routes
 app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+app.use('/links', require('./routes/links'));
+
 //Public files
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Starting server
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,5 +6,6 @@ router.get('/', (req, res) => {
 });
 
 
+
 module.exports = router;
 
