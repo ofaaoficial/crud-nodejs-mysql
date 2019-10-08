215aa3ee06590873bb31c016b22f357a3bbf514e
ofaaoficial <oscarfamado@gmail.com>
2019-10-08 18:07:00 -0500

    start config of view engine

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 //Initializations
 const app = express();
 
 //Configurations
 app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname , 'views'));
+
+
+//motor de vistas
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir:  path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
 
 //Middlewares - Security
 app.use(morgan('dev'));
@@ -15,7 +29,6 @@ app.use(morgan('dev'));
 //Routes
 app.use(require('./routes'));
 //Public files
-
 
 //Starting server
 
