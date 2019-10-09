a8d4bc5e74d115d58275a279e5225b61f4255971
ofaaoficial <oscarfamado@gmail.com>
2019-10-09 17:31:10 -0500

    creating routes and views of sign up

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/sign-up', (req, res) => {
+    res.render('auth/signup.hbs');
+})
+
+router.post('/sign-up', (req, res) => {
+    console.log(req.body);
+})
+
 module.exports = router;
