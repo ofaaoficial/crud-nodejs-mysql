6c8059f9e540d20fa3c70a1984e2c381d322d51d
ofaaoficial <oscarfamado@gmail.com>
2019-10-08 19:26:41 -0500

    create views and integrate partials with hbs

--- src/routes/books.js
+++ src/routes/books.js
@@ -3,5 +3,12 @@ const router = express.Router();
 
 const pool = require('../database');
 
+router.get('/create', (req, res) => {
+    res.render('books/create');
+});
+
+router.post('/create', (req, res) => {
+    res.send('received');
+})
 
 module.exports = router;
